b69ee69a5d702d64769a0303b9b61acd79531d19
TinaTiresome <113856966+TinaTiresome@users.noreply.github.com>
2023-07-09 17:30:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,133 +1,156 @@
 import os
+from config import TEMP_FOLDER_NAME
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-import time
+from story_engine import StoryEngine, StoryItem, StoryItemType
+from helpers import is_image, is_any_document
+import base64
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-## App Layout
-app.layout = html.Div([
-    html.Div([
-        html.Div([
-            # Upload Files element
-            dcc.Upload(
-                id='upload-data',
-                children=html.Div([
-                    'Drag and Drop or ',
-                    html.A('Select Files')
-                ]),
-                className='upload-data',
-                multiple=True
-            ),
-            # Uploaded Files or 'no files uploaded' text
-            html.Div(id='file-upload-content'),
-            # Upload progress bar
-            dbc.Progress(id='upload-progress', value=0, max=100, className='upload-progress'),
-            # Additional Inputs
-            html.Div([
-                # Genre Dropdown
-                dcc.Dropdown(
-                    id='genre-dropdown',
-                    options=[
-                        {'label': 'Horror', 'value': 'horror'},
-                        {'label': 'Fantasy', 'value': 'fantasy'},
-                        {'label': 'Romance', 'value': 'romance'},
-                        # Add more genres as needed
+app.layout = html.Div(
+    children=[
+        html.Header(
+            children=[
+                html.H1('Storyboard'),
+            ]
+        ),
+        html.Div(
+            children=[
+                html.Div([
+                    html.Div([
+                        # Genre Dropdown
+                        dcc.Dropdown(
+                            id='genre-dropdown',
+                            options=[
+                                {'label': 'Horror', 'value': 'horror'},
+                                {'label': 'Fantasy', 'value': 'fantasy'},
+                                {'label': 'Romance', 'value': 'romance'},
+                            ],
+                            placeholder="Select a genre"
+                        ),
+                        dcc.Dropdown(
+                            id='author-dropdown',
+                            options=[
+                                {'label': 'Agatha Christie', 'value': 'Agatha Christie'},
+                                {'label': 'Jane Austen', 'value': 'Jane Austen'},
+                                {'label': 'Stephen King', 'value': 'Stephen King'},
+                            ],
+                            placeholder="Select an Author"
+                        ),
                     ],
-                    placeholder="Select a genre",
-                    className='genre-dropdown'
-                ),
-            ], id='input-options', className='input-options'),
-            # Generate Story Button
-            dbc.Button("Generate Story", color="primary", id='generate-button', className="m-3", disabled=True),
-            # Download Result
-            dcc.Download(id="download-result"),
-            # Download Result Button
-            dbc.Button("Download Result", color="primary", id='download-button', className="m-3"),
-            # Generation Progress bar
-            dbc.Progress(id='generation-progress', value=0, max=100, className='generation-progress'),
-            # Faking the progress at the moment (i think)
-            dcc.Interval(id='generation-interval', interval=2000, n_intervals=0),
-        ], className="d-flex flex-column align-items-center")
-    ])
-])
+                    className='input-options'),
+                    # Upload Files element
+                    dcc.Upload(
+                        id='upload-data',
+                        children=html.Div([
+                            'Drag and Drop or ',
+                            html.A('Select Files')
+                        ]),
+                        multiple=True,
+                        className='upload-data'
+                    ),
+                    # Uploaded Files or 'no files uploaded' text
+                    html.Div(id='file-upload-content'),
+                    # Progress bar
+                    dbc.Progress(id='progress-bar', value=0, max=100),
+                    # Additional Inputs
+                    # Generate Story Button
+                    dbc.Button("Download Story", color="primary", id='download-button', className="m-3", disabled=True),
+                    # Download Result
+                    dcc.Download(id="download-result"),
+                ], 
+                # Formatting things
+                className="d-flex flex-column align-items-center"
+                )
+            ]
+        ),
+    ]
+)
 
-# Your callback functions remain unchanged...
+# Store total progress outside the callback context
+total_progress = 0
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
-## Functions
-# Update Output
-#
 @app.callback(
     Output('file-upload-content', 'children'),
-    Output('upload-progress', 'value'),
-    Output('generate-button', 'disabled'),
+    Output('progress-bar', 'value'),
+    Output('download-button', 'disabled'),
     Input('upload-data', 'contents'),
+#    Input('genre-dropdown','children'), #Input from genre dropdown
+#    Input('author-dropdown', 'children'), #input from author dropdown
     State('upload-data', 'filename'),
     State('upload-data', 'last_modified')
 )
-def update_output(contents, filenames, last_modified):
-    if not os.path.exists('temp'):
-        os.makedirs('temp')
+def update_output(contents, filenames, last_modified): #add genre and author
+    global total_progress
+    if not os.path.exists(TEMP_FOLDER_NAME):
+        os.makedirs(TEMP_FOLDER_NAME)
 
     if contents is not None:
         total_files = len(contents)
         uploaded_files = 0
         file_contents = []
 
+        story_engine = StoryEngine("fantasy")# add genre and author
+
+        story_data_path = os.path.join(TEMP_FOLDER_NAME, story_engine.story_id)
+        print(story_data_path)
+        os.mkdir(story_data_path)
+
         for content, filename, date in zip(contents, filenames, last_modified):
             if content is not None:
                 # Save the file to a temporary location
-                data = content.encode('utf-8')
-                filepath = os.path.join('temp', filename)
+                data = base64.b64decode(content.split(",")[1])
+                filepath = os.path.join(story_data_path, filename)
+
                 with open(filepath, 'wb') as f:
                     f.write(data)
 
+                # Filtering out only the accepted story type file formats.
+                storytype: StoryItemType
+                if is_image(filename):
+                    storytype = StoryItemType.IMAGE
+                elif is_any_document(filename):
+                    storytype = StoryItemType.DOCUMENT
+                else:
+                    continue
+
+                story_engine.addItem(StoryItem(filepath, filename, storytype, date))
                 file_contents.append(html.P(filename))
                 uploaded_files += 1
 
-        upload_progress = int(uploaded_files / total_files * 100) if total_files > 0 else 0
+                # Update upload progress (50% of total progress)
+                total_progress = (uploaded_files / total_files) * 50
+                print(f"Upload Progress: {total_progress}%")
+
+        def story_progress_callback(p: float):
+            global total_progress
+            # Update story generation progress (50% of total progress)
+            total_progress = 50 + (p * 50)
+            print(f"Total Progress: {total_progress}%")
+            return total_progress
+
+        story = story_engine.once_upon_a_time(story_progress_callback)
+        story_path = os.path.join(story_data_path, story_engine.story_title + '.txt')
+
+        with open(story_path, 'w') as f:
+            f.write(story)
 
         if file_contents:
-            return file_contents, upload_progress, False  # Enable the Generate button
+            return file_contents, total_progress, False  # Enable the Generate button
 
     return 'No files uploaded.', 0, True  # Disable the Generate button
-
-# Generation Progress
-total_progress = 0  # Store total progress outside the callback context
-
-@app.callback(
-    Output('generation-progress', 'value'),
-    Output('generation-progress', 'children'),
-    Input('generation-interval', 'n_intervals'),
-    State('generate-button', 'n_clicks')
-)
-def update_generation_progress(n_intervals, n_clicks):
-    global total_progress
-
-    if n_clicks:
-        if total_progress < 100:
-            time.sleep(1)  # Simulate delay
-            total_progress += 10
-
-        return total_progress, f'{total_progress}%'
-    
-    return 0, ''
 
 # Download Result
 @app.callback(
     Output('download-result', 'data'),
     Input('download-button', 'n_clicks'),
-    State('generation-progress', 'value')
+    State('progress-bar', 'value')
 )
-def download_result(n_clicks, generation_progress):
-    if n_clicks and generation_progress == 100:
-        # Placeholder code to simulate the generated story
+def download_result(n_clicks, total_progress):
+    if n_clicks and total_progress == 100:
         story = "This is the generated story."
 
         # Return the result for download
