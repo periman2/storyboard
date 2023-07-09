132595889fdaa5cefc07c7b5af4c74675b8c541a
user <periklis@vitrinabox.com>
2023-07-09 18:05:29 +0300

    storyboard: testing new deployment credential strategy

--- ai_text.py
+++ ai_text.py
@@ -6,11 +6,31 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.document_loaders import Docx2txtLoader, UnstructuredWordDocumentLoader, PDFPlumberLoader, TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
+import os
+import json
 
 from typing import List, Any
 
 from dotenv import load_dotenv
 load_dotenv()
+
+credentials = {
+    "type": os.environ.get('GAC_TYPE'),
+    "project_id": os.environ.get('GAC_PROJECT_ID'),
+    "private_key_id": os.environ.get('GAC_PRIVATE_KEY_ID'),
+    "private_key": os.environ.get('GAC_PRIVATE_KEY'),
+    "client_email": os.environ.get('GAC_CLIENT_EMAIL'),
+    "client_id": os.environ.get('GAC_CLIENT_ID'),
+    "auth_uri": os.environ.get('GAC_AUTH_URI'),
+    "token_uri": os.environ.get('GAC_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.environ.get('GAC_AUTH_CERT_URI'),
+    "client_x509_cert_url": os.environ.get('GAC_CLIENT_CERT_URL'),
+    "universe_domain": os.environ.get('GAC_UNIVERSE_DOMAIN')
+}
+
+filename = "credentials.json"
+with open(filename, "w") as json_file:
+    json.dump(credentials, json_file, indent=4)
 
 text_splitter = CharacterTextSplitter(chunk_size=900, separator="")
 
