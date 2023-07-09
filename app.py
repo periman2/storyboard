437bb4aae2a05638bec0cf0c4fda971dba5d13b0
user <periklis@vitrinabox.com>
2023-07-09 04:47:53 +0300

    Storyboard: First working stories!

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import os
+from config import TEMP_FOLDER_NAME
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import time
+from story_engine import StoryEngine, StoryItem, StoryItemType
+from typing import List
+from ai_text import exectuteTextAIPrompt, executeDocumentSummarization
+from ai_image import getDescriptionsOfImage
+from helpers import is_doc, is_image, is_pdf, is_txt, is_any_document
+import base64
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -114,30 +121,55 @@ app.layout = html.Div([
     State('upload-data', 'last_modified')
 )
 def update_output(contents, filenames, last_modified):
-    if not os.path.exists('temp'):
-        os.makedirs('temp')
+    if not os.path.exists(TEMP_FOLDER_NAME):
+        os.makedirs(TEMP_FOLDER_NAME)
 
     if contents is not None:
         total_files = len(contents)
         uploaded_files = 0
         file_contents = []
 
+        story_engine = StoryEngine("fantasy")
+
+        story_data_path = os.path.join(TEMP_FOLDER_NAME, story_engine.story_id)
+        
+        print(story_data_path)
+        os.mkdir(story_data_path)
+
         for content, filename, date in zip(contents, filenames, last_modified):
             if content is not None:
                 # Save the file to a temporary location
-                print("Yayaaaaaaaaa this works!")
-                data = content.encode('utf-8')
-                filepath = os.path.join('temp', filename)
+                data = base64.b64decode(content.split(",")[1])
+                filepath = os.path.join(story_data_path, filename)
+
                 with open(filepath, 'wb') as f:
                     f.write(data)
+
+                #Filtering out only the accepted story type file formats. However this should be done before arriving at the backend normally. (TODO: Priority 3)
+                storytype: StoryItemType
+                if is_image(filename):
+                    storytype = StoryItemType.IMAGE
+                elif is_any_document(filename):
+                    storytype = StoryItemType.DOCUMENT
+                else:
+                    continue
+
+                story_engine.addItem(StoryItem(filepath, filename, storytype, date))
 
                 file_contents.append(html.P(filename))
                 uploaded_files += 1
 
+        story = story_engine.onec_upon_a_time()
+        
+        story_path = os.path.join(story_data_path, story_engine.story_title + '.txt')
+        
         upload_progress = int(uploaded_files / total_files * 100) if total_files > 0 else 0
 
+        with open(story_path, 'w') as f:
+            f.write(story)
+            
         if file_contents:
-            return file_contents, upload_progress, False  # Enable the Generate button
+            return story  # Enable the Generate button
 
     return 'No files uploaded.', 0, True  # Disable the Generate button
 
---
+++ config.py
@@ -0,0 +1,5 @@
+TEMP_FOLDER_NAME='temp'
+VERTEX_AI_TEMP=0.5
+LABELS_MAP_PATH='resources/imagenet21k_labels.txt'
+IMAGE_MODEL_URL="https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_s/classification/2"
+IMAGE_MODEL_IMAGE_SIZE=384
